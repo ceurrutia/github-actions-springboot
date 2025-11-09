62ffaa8702fdfcd8ae38d74329d584e7db696f68
Cecilia Urrutia <urrutia.cici@gmail.com>
2025-11-09 13:23:42 -0300

    initial commit

---
+++ src/main/java/com/proyectoSpring/api_rest/ApiRestApplication.java
@@ -0,0 +1,21 @@
+package com.proyectoSpring.api_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ApiRestApplication {
+
+	@GetMapping("/welcome")
+	public String saludar(){
+		return "Hi, baby";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestApplication.class, args);
+	}
+
+}
