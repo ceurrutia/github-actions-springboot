ad437c3885804562d741ce64b74f8dc377f5cc36
Cecilia Urrutia <urrutia.cici@gmail.com>
2025-11-10 14:28:32 -0300

    upload lista productos

--- src/main/java/com/proyectoSpring/api_rest/ApiRestApplication.java
+++ src/main/java/com/proyectoSpring/api_rest/ApiRestApplication.java
@@ -5,23 +5,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class ApiRestApplication {
 
 	@GetMapping("/welcome")
 	public String saludar(){
-		return "Hi, baby baby";
+		return "Hi, baby";
 	}
 
 	@GetMapping("/status")
-	public String testeo(){
-		return "Aqui estara el status de sistema";
+	public String status(){
+		return "System UP";
 	}
 
 	@GetMapping("/data")
-	public String about(){
-		return "Aqui recibira y procesara datos";
+	public List<Product> todosLosProductos(){
+		List<Product> productos = new ArrayList<>();
+		productos.add(new Product(1, "PC", "Laptop Dell 5486", 29.96, 3, true));
+		productos.add(new Product(2, "Mouse", "Mouse inalámbrico Logitech", 10.50, 15, true));
+		return productos;
 	}
 
 	public static void main(String[] args) {
